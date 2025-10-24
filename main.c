5e4e383a748a1fdd3624a4b87a50bb773d1c17a5
TAS Tom <tom.tas6@etu.univ-lorraine.fr>
2025-10-24 14:49:55 +0200

    ajout du path + exécution des prompts en cours

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+
+
 typedef enum
 {
     MOT,
@@ -91,26 +97,40 @@ static LEX getlex(char *mot)
 int main(int argc, char *argv[])
 {
     char mot[200];
+    //machine actuelle HOSTNAME + répertoire courant + $
+    char * path = getcwd(NULL, 0);
+    printf("%s$ ", path);
+
     while (1)
+        
         switch (getlex(mot))
         {
         case MOT:
             printf("MOT: %s\n", mot);
+            execvp(mot, NULL); //exécuter la commande
+         
             break;
         case TUB:
             printf("TUBE\n");
+          
             break;
         case INF:
             printf("REDIRECTION ENTREE\n");
+          
             break;
         case SUP:
             printf("REDIRECTION SORTIE\n");
+          
             break;
         case SPP:
             printf("REDIRECTION AJOUT\n");
+            
             break;
         case NL:
             printf("NOUVELLE LIGNE \n");
+            //exécuter la commande
+            
+            printf("%s$ ", path);
             break;
         case FIN:
             printf("FIN \n");
@@ -118,4 +138,101 @@ int main(int argc, char *argv[])
         }
     return 0;
 
+}
+
+//Fonction qui traite une commande
+void commande(char *argv[])
+{
+    enum
+    {
+        INF,
+        SUP,
+        SPP,
+        TUB,
+    } etat = INF;
+    //cas entrée par defaut fd=0
+    int fd = 0;
+    for (int i = 0; argv[i] != NULL; i++)
+    {
+        switch (etat)
+        {
+        case INF:
+            if (strcmp(argv[i], "<") == 0)
+            {
+                etat = SPP;
+                continue;
+            }
+            else
+            {
+                fd = open(argv[i], O_RDONLY);
+                dup2(fd, 0);
+                close(fd);
+            }
+            break;
+        case SUP:
+            if (strcmp(argv[i], ">") == 0)
+            {
+                etat = SPP;
+                continue;
+            }
+            else
+            {
+                fd = open(argv[i], O_WRONLY
+                                | O_CREAT
+                                | O_TRUNC,
+                            0644);
+                dup2(fd, 1);
+                close(fd);
+            }
+            break;
+        case SPP:
+            if (strcmp(argv[i], ">") == 0)
+            {
+                etat = TUB;
+                continue;
+            }
+            else
+            {
+                fd = open(argv[i], O_WRONLY
+                                | O_CREAT
+                                | O_APPEND,
+                            0644);
+                dup2(fd, 1);
+                close(fd);
+            }
+            break;
+        case TUB:
+            if (strcmp(argv[i], "|") == 0)
+            {
+                etat = INF;
+                continue;
+            }
+            else
+            {
+                int pid = fork();
+                if (pid == 0)
+                {
+                    //fils
+                    dup2(fd, 0);
+                    close(fd);
+                    execlp(argv[i], argv[i], NULL);
+                    exit(0);
+                }
+                else
+                {
+                    //pere
+                    wait(NULL);
+                    close(fd);
+                    int pipefd[2];
+                    pipe(pipefd);
+                    fd = pipefd[0];
+                    dup2(pipefd[1], 1);
+                    close(pipefd[1]);
+                }
+            }
+            break;
+        }
+    }
+    execlp(argv[0], argv[0], NULL);
+    exit(0);
 }
